d242aa1fa379faa96b24ff79d81e71929b029213
BHC <oussamabhc@192.168.1.20>
2025-03-24 10:28:01 +0000

    Ajout de la configuration Spring Security avec authentification en mémoire (InMemomy Authentication)

--- src/main/java/ma/enset/hopital/HopitalApplication.java
+++ src/main/java/ma/enset/hopital/HopitalApplication.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -21,9 +24,9 @@ public class HopitalApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        patientRepository.save(new Patient(null, "Mohammed", new Date(), false, 134));
+/*        patientRepository.save(new Patient(null, "Mohammed", new Date(), false, 134));
         patientRepository.save(new Patient(null, "Hanane", new Date(), false, 432));
-        patientRepository.save(new Patient(null, "Imane", new Date(), true, 156));
+        patientRepository.save(new Patient(null, "Imane", new Date(), true, 156));*/
 
 /*        Patient patient = new Patient();
         patient.setId(null);
@@ -35,5 +38,10 @@ public class HopitalApplication implements CommandLineRunner {
         Patient patient2 = new Patient(null, "Yassine", new Date(), false, 123);
 
         Patient patient3 = Patient.builder().nom("Imane").dateNaissance(new Date()).malade(true).score(56).build();*/
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
