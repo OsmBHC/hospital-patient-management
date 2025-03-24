c920dd519ada96686558125884e8a0f45d57218b
BHC <oussamabhc@192.168.1.20>
2025-03-24 12:09:33 +0000

    Ajout d'un login personnalisé avec sécurité et gestion des accès

--- src/main/java/ma/enset/hopital/web/PatientController.java
+++ src/main/java/ma/enset/hopital/web/PatientController.java
@@ -6,6 +6,7 @@ import ma.enset.hopital.entities.Patient;
 import ma.enset.hopital.repository.PatientRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -39,6 +40,7 @@ public class PatientController {
     }
 
     @GetMapping("/admin/delete")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String delete(@RequestParam(name = "id") Long id,
                          @RequestParam(name = "keyword",defaultValue = "") String keyword,
                          @RequestParam(name = "page",defaultValue = "0") int page){
@@ -47,12 +49,14 @@ public class PatientController {
     }
 
     @GetMapping("/admin/formPatients")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String formPatients(Model model){
         model.addAttribute("patient",new Patient());
         return "formPatients";
     }
 
     @PostMapping(path="/admin/save")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String save(Model model, @Valid Patient patient, BindingResult bindingResult,
                        @RequestParam(defaultValue = "0") int page,
                        @RequestParam(defaultValue = "") String keyword){
@@ -62,6 +66,7 @@ public class PatientController {
     }
 
     @GetMapping("/admin/editPatient")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String editPatient(Model model, Long id, int page, String keyword) {
         Patient patient = patientRepository.findById(id).orElse(null);
         if (patient == null) throw new RuntimeException("Patient introuvable.");
--- src/main/java/ma/enset/hopital/web/SecurityController.java
+++ src/main/java/ma/enset/hopital/web/SecurityController.java
@@ -9,4 +9,9 @@ public class SecurityController {
     public String notAuthorized(){
         return "notAuthorized";
     }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
 }
