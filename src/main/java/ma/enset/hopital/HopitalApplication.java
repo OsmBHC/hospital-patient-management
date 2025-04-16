5286a4206b6a0ab79e7a617cf56d431e9b969244
BHC <oussamabhc@192.168.1.18>
2025-04-17 00:46:07 +0100

    Ajout de la sécurité JDBC avec gestion d'utilisateurs, logout et seed des utilisateurs

--- src/main/java/ma/enset/hopital/HopitalApplication.java
+++ src/main/java/ma/enset/hopital/HopitalApplication.java
@@ -7,13 +7,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import java.util.Date;
 
 @SpringBootApplication
-public class HopitalApplication implements CommandLineRunner {
+public class HopitalApplication {
 
     @Autowired
     private PatientRepository patientRepository;
@@ -22,13 +25,13 @@ public class HopitalApplication implements CommandLineRunner {
         SpringApplication.run(HopitalApplication.class, args);
     }
 
-    @Override
+/*    @Override
     public void run(String... args) throws Exception {
-/*        patientRepository.save(new Patient(null, "Mohammed", new Date(), false, 134));
+*//*        patientRepository.save(new Patient(null, "Mohammed", new Date(), false, 134));
         patientRepository.save(new Patient(null, "Hanane", new Date(), false, 432));
-        patientRepository.save(new Patient(null, "Imane", new Date(), true, 156));*/
+        patientRepository.save(new Patient(null, "Imane", new Date(), true, 156));*//*
 
-/*        Patient patient = new Patient();
+*//*        Patient patient = new Patient();
         patient.setId(null);
         patient.setNom("Mohammed");
         patient.setDateNaissance(new Date());
@@ -37,7 +40,44 @@ public class HopitalApplication implements CommandLineRunner {
 
         Patient patient2 = new Patient(null, "Yassine", new Date(), false, 123);
 
-        Patient patient3 = Patient.builder().nom("Imane").dateNaissance(new Date()).malade(true).score(56).build();*/
+        Patient patient3 = Patient.builder().nom("Imane").dateNaissance(new Date()).malade(true).score(56).build();*//*
+    }*/
+
+    @Bean
+    CommandLineRunner start(PatientRepository patientRepository) {
+        return args -> {
+            for (int i = 0; i < 10; i++) {
+                patientRepository.save(new Patient(null, "Mohammed", new Date(), false, 134));
+                patientRepository.save(new Patient(null, "Hanane", new Date(), false, 432));
+                patientRepository.save(new Patient(null, "Imane", new Date(), true, 156));
+                patientRepository.save(new Patient(null, "Yassine", new Date(), false, 123));
+            }
+        };
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager) {
+        PasswordEncoder passwordEncoder = passwordEncoder();
+        return args -> {
+            UserDetails u1 = jdbcUserDetailsManager.loadUserByUsername("user11");
+            if (u1 == null) {
+                jdbcUserDetailsManager.createUser(
+                        User.withUsername("user11").password(passwordEncoder.encode("1234")).roles("USER").build()
+                );
+            }
+            UserDetails u2 = jdbcUserDetailsManager.loadUserByUsername("user22");
+            if (u2 == null) {
+                jdbcUserDetailsManager.createUser(
+                        User.withUsername("user22").password(passwordEncoder.encode("1234")).roles("USER").build()
+                );
+            }
+            UserDetails u3 = jdbcUserDetailsManager.loadUserByUsername("admin2");
+            if (u3 == null) {
+                jdbcUserDetailsManager.createUser(
+                        User.withUsername("admin2").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN").build()
+                );
+            }
+        };
     }
 
     @Bean
--- src/main/java/ma/enset/hopital/security/SecurityConfig.java
+++ src/main/java/ma/enset/hopital/security/SecurityConfig.java
@@ -9,7 +9,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -17,7 +20,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Autowired
     private PasswordEncoder passwordEncoder;
+
     @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    //@Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
         return new InMemoryUserDetailsManager(
                 User.withUsername("user1").password(passwordEncoder.encode("1234")).roles("USER").build(),
@@ -28,13 +37,18 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.formLogin().loginPage("/login").permitAll();
+        httpSecurity.formLogin().loginPage("/login").defaultSuccessUrl("/", true).permitAll();
         httpSecurity.rememberMe();
         httpSecurity.authorizeHttpRequests().requestMatchers("/webjars/**", "/h2-console/**").permitAll();
         // httpSecurity.authorizeHttpRequests().requestMatchers("/user/**").hasRole("USER");
         // httpSecurity.authorizeHttpRequests().requestMatchers("/admin/**").hasRole("ADMIN");
         httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
         httpSecurity.exceptionHandling().accessDeniedPage("/notAuthorized");
+        httpSecurity.logout()
+                .logoutUrl("/logout") // URL de déconnexion
+                .logoutSuccessUrl("/login?logout") // Redirection après déconnexion
+                .invalidateHttpSession(true) // Invalider la session
+                .deleteCookies("JSESSIONID", "remember-me"); // Supprimer les cookies
         return httpSecurity.build();
     }
 }
